46cfe97a01c7fd2551b981224b27df7640d8e082
mihir <mihir231216@gmail.com>
2023-12-30 13:40:26 +0530

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Based on individual trip attributes, participants should predict the duration of each trip in the test set.',
+    author='Mihir',
+    license='',
+)
